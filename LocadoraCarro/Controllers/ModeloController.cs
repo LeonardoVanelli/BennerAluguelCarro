6f9d90edc4de87909ba23db1876bab742fca1a38
Leonardo Vanelli <30638801+LeonardoVanelli@users.noreply.github.com>
2017-10-24 16:13:16 -0200

    Index

---
+++ LocadoraCarro/Controllers/ModeloController.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraCarro.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocadoraCarro.Controllers
+{
+    public class ModeloController : Controller
+    {
+        // GET: Modelo
+        public ActionResult Index()
+        {
+            var dao = new ModeloDAO();
+            ViewBag.Modelos = dao.Lista();
+            return View();
+        }
+    }
+}
