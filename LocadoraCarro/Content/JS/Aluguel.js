b6f16234facf64e1b99184abef75ab16cae0766a
Leonardo Vanelli <30638801+LeonardoVanelli@users.noreply.github.com>
2017-11-01 15:41:35 -0200

    TransacaoBasica

---
+++ LocadoraCarro/Content/JS/Aluguel.js
@@ -0,0 +1,24 @@
+﻿var FormCarro = $(".carro");
+var FormProtecao = $(".protecao");
+var FormCliente = $(".cliente");
+
+$("#btn-proximo").click(function () {
+    event.preventDefault();    
+    var FormData = $(".data-hora");
+
+FormData.addClass("invisivel");
+FormCarro.removeClass("invisivel");
+
+})
+
+$("#btnSlcCarro").click(function () {
+    event.preventDefault();
+    FormCarro.addClass("invisivel");
+    FormProtecao.removeClass("invisivel");
+})
+
+$("#btnSlcProtecao").click(function () {
+    event.preventDefault();
+    FormProtecao.addClass("invisivel");
+    FormCliente.removeClass("invisivel");
+})
