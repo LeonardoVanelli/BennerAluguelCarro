711fffb52946b35ddc499994363479654972cbbd
Leonardo Vanelli <30638801+LeonardoVanelli@users.noreply.github.com>
2017-11-26 15:17:24 -0200

    Home Page adm

--- LocadoraCarro/Controllers/HomeController.cs
+++ LocadoraCarro/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿
 using LocadoraCarro.DAO;
+using LocadoraCarro.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,54 @@ namespace LocadoraCarro.Controllers
     {
         // GET: Home
         public ActionResult Index()
-        {            
-            var funcionarioLogado = new FuncionarioDAO().BuscaPorId(4);
-            ViewBag.Funcionario = funcionarioLogado;
+        {
+            if (Session["funcionarioLogado"] != null)
+            {
+                if (((Funcionario)(Session["funcionarioLogado"])).FuncaoId != 1)
+                {
+                    return RedirectToAction("AIndex");
+                }
+                else
+                {
+                    return RedirectToAction("AIndex");
+                }
+            }
+            else
+            {
+                return View();
+            }                              
+        }
 
+        public ActionResult AIndex()
+        {
+            var alugueis = new AluguelDAO().Lista();
+            var numeroDeRetiradasHoje = 0;
+            var numeroDeCancelados = 0;
+            var numeroRetirados = 0;
+            var numeroNaoRetirados = 0;
+            foreach (var aluguel in alugueis)
+            {
+                if (aluguel.DataHoraRetirada.ToString("dd/MM/yyyy") == DateTime.Now.ToString("dd/MM/yyyy"))
+                {
+                    numeroDeRetiradasHoje++;
+                    if (aluguel.StatusId == new StatusDAO().BuscaPorNome("Cancelado").Id)
+                    {
+                        numeroDeCancelados++;
+                    }
+                    if (aluguel.StatusId == new StatusDAO().BuscaPorNome("Retirado").Id)
+                    {
+                        numeroRetirados++;
+                    }
+                    if (aluguel.StatusId == new StatusDAO().BuscaPorNome("Nao Retirado").Id)
+                    {
+                        numeroNaoRetirados++;
+                    }
+                }
+            }          
+            ViewBag.NumeroRetiradas = numeroDeRetiradasHoje;
+            ViewBag.numeroCancelados = numeroDeCancelados;
+            ViewBag.NumerosRetirados = numeroRetirados;
+            ViewBag.numeroNaoRetirados = numeroNaoRetirados;
             return View();
         }
     }
